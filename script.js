90fe83d6781dff2549cfc8ff8757e79cc5cf2a7b
aditya <aditya.s.baghel1@gmail.com>
2020-09-27 23:29:07 +0530

    Adding js

---
+++ script.js
@@ -0,0 +1,14 @@
+const APIURL = "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=04c35731a5ee918f014970082a0088b1&page=1"
+const IMGPATH = "https://image.tmdb.org/t/p/w1280";
+
+async function getMovies(){
+    const resp = await fetch(APIURL);
+    const respData = await resp.json();
+
+
+    console.log(respData);
+
+    return respData;
+}
+
+getMovies();
